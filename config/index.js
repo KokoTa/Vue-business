e13054d7cc12325514bc10da9eb39f46afd6ede5
KokoTa <584847514@qq.com>
2017-10-10 00:51:47 +0800

    use axios | fix index page | write the carousel

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/news': {
+            target: 'https://news-at.zhihu.com/api/4/news',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/news': '/latest' // 等于/news对应的地址加上/latest
+            }
+        }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,14 +17,15 @@
 			<div class="index-left-block">
 				<h1>Cool news</h1>
 				<ul>
-					<li v-for="item in newList">
-						<a :href="item.url" target="_blacnk">{{ item.title }}</a>
+					<li v-for="(item, index) in newList" class="news-hidden">
+						<a :href="'http://daily.zhihu.com/story/' + item.id" target="_blacnk">{{ index + 1}}. {{ item.title }}</a>
 					</li>
 				</ul>
 			</div>
 		</div>
 		<!-- right -->
 		<div class="index-right">
+			<slider :slideList="slideList"></slider>
 			<div class="index-board">
 				<div class="index-board-item "
 				v-for="(item, index) in boardList">
@@ -40,7 +41,12 @@
 </template>
 
 <script>
+	import slider from '@/components/slider'
+
 	export default {
+		components: {
+			slider
+		},
 		name: 'index',
 		data () {
 			return {
@@ -91,18 +97,7 @@
 					}
 				},
 				newList: [
-					{
-						title: '今天的新闻01',
-						url: 'http://baidu.com'
-					},
-					{
-						title: '今天的新闻02',
-						url: 'http://baidu.com'
-					},
-					{
-						title: '今天的新闻03',
-						url: 'http://baidu.com'
-					}
+
 				],
 				boardList: [
 					{
@@ -127,12 +122,37 @@
 					}
 				],
 				slideList: [
-
+					{
+						title: '01',
+						src: require('@/assets/img/s1.jpg'),
+						href: 'www.baidu.com'
+					},
+					{
+						title: '02',
+						src: require('@/assets/img/s2.jpg'),
+						href: 'www.baidu.com'
+					},
+					{
+						title: '03',
+						src: require('@/assets/img/s3.jpg'),
+						href: 'www.baidu.com'
+					},
+					{
+						title: '04',
+						src: require('@/assets/img/s4.jpg'),
+						href: 'www.baidu.com'
+					}
 				]
 			}
 		},
 		created: function() {
-
+			this.axios.get('/news')
+				.then(data => {
+					this.newList = data.data.stories;
+				})
+				.catch(error => {
+					console.log(error);
+				})
 		}
 	}
 </script>
@@ -166,10 +186,15 @@
 				ul {
 					padding: 1rem;
 					li {
-						padding: 5px;
+						padding-bottom: .5rem;
+						&:last-child {
+							padding-bottom: 0;
+						}
 						a {
 							text-decoration: none;
 							color: gray;
+							line-height: 1.2rem;
+							font-size: .8rem;
 						}
 						a:hover {
 							color: black;
@@ -180,7 +205,13 @@
 						background: red;
 						color: white;
 						border-radius: 5px;
-						padding: 3px;
+						padding: 0 2px;
+					}
+					.news-hidden {
+						width: 100%;
+						white-space: nowrap;
+						overflow: hidden;
+						text-overflow: ellipsis;
 					}
 				}
 			}
@@ -189,37 +220,42 @@
 			float: left;
 			width: 900px;
 			margin-top: 1rem;
-			.index-board-item {
-				float: left;
-				width: 400px;
-				padding: 1rem;
-				background: white;
-				margin: 0 5px 5px 0;
-				.board-item-bg {
-					padding-left: 7.5rem;
-					background: url(../assets/img/1.png) no-repeat;
-					h1, p, button {
-						margin-bottom: 1rem;
+			.index-board {
+				.index-board-item {
+					display: inline-block;
+					width: 400px;
+					padding: 20px;
+					background: white;
+					margin: 0 20px 20px 0;
+					&:nth-child(2n) {
+						margin-right: 0;
 					}
-					h1 {
-						font-size: 1.5rem;
-						font-weight: bolder;
-					}
-					p {
-						font-size: .8rem;
-						color: gray;
-					}
-					button {
-						width: 8rem;
-						height: 2rem;
-						border: none;
-						background: black;
-						color: white;
-						box-shadow: .2rem .2rem 1rem gray;
-						cursor: pointer;
-					}
-					button:hover {
-						background: gray;
+					.board-item-bg {
+						padding-left: 120px;
+						background: url(../assets/img/1.png) no-repeat;
+						h1, p, button {
+							margin-bottom: 1rem;
+						}
+						h1 {
+							font-size: 1.5rem;
+							font-weight: bolder;
+						}
+						p {
+							font-size: .8rem;
+							color: gray;
+						}
+						button {
+							width: 8rem;
+							height: 2rem;
+							border: none;
+							background: black;
+							color: white;
+							box-shadow: .2rem .2rem 1rem gray;
+							cursor: pointer;
+						}
+						button:hover {
+							background: gray;
+						}
 					}
 				}
 			}
